d4353bce897d49f52e0a299cf01368af4c4163ea
rizaton <123567427+rizaton@users.noreply.github.com>
2024-04-18 02:17:20 +0700

    Second Program Base

---
+++ app/Validation/admin/StockValidate.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Validation;
+
+class StockValidate
+{
+    // public function custom_rule(): bool
+    // {
+    //     return true;
+    // }
+}
---
+++ app/Validation/user/ForgotPasswordValidate.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Validation;
+
+class ForgotPasswordValidate
+{
+    // public function custom_rule(): bool
+    // {
+    //     return true;
+    // }
+}
