b1c71b23b45b512d0969770e0f2d966d5a2fb9f0
rizaton <123567427+rizaton@users.noreply.github.com>
2024-04-18 11:21:01 +0700

    feat(views): header & footer modified

---
+++ app/Views/partisans/header.php
@@ -0,0 +1,9 @@
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="<?= base_url() ?>assets/css/styles.css" rel="stylesheet">
+</head>
+
+<body>
