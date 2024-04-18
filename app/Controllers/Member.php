7637c83176f3952f7e924f72b20930f68bf991a9
rizaton <123567427+rizaton@users.noreply.github.com>
2024-04-18 11:44:42 +0700

    feat(controllers): Admin, Login & Members

---
+++ app/Controllers/Member.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+use CodeIgniter\HTTP\ResponseInterface;
+
+class Member extends BaseController
+{
+    public function index()
+    {
+        //
+    }
+}
