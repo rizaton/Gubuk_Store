95fbb7e91e5e86ec0724dd6ded4f1e7363f117f6
FikalAhmad <fikaalm26@gmail.com>
2024-04-23 00:14:02 +0700

    chore: add navbar user and member mode

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -4,8 +4,10 @@ namespace App\Controllers;
 
 class Home extends BaseController
 {
-    public function index(): string
+    public function index()
     {
-        return view('testing');
+        echo view('partisans/header');
+        echo view('partisans/navbar_member');
+        echo view('partisans/footer');
     }
 }
