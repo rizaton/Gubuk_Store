c22a91e6a14f8e6b9cbaa121a614000a1cdb69fd
rizaton <123567427+rizaton@users.noreply.github.com>
2024-04-24 06:58:41 +0900

    feat(views): adding products view

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,7 +6,7 @@ class Home extends BaseController
 {
     public function index()
     {
-        return view('admin/index');
+        return view('user/products', ['templates' => 'main_user']);
         // echo view('partisans/header');
         // echo view('partisans/navbar_member');
         // echo view('partisans/footer');
