fe6cf64ae0064fcc4884dd204c8d541c077bd4c3
rizaton <123567427+rizaton@users.noreply.github.com>
2024-04-23 11:55:22 +0700

    feat(models): adding CRUD functions in each models

--- app/Models/UserManagementModel.php
+++ app/Models/UserManagementModel.php
@@ -43,4 +43,21 @@ class UserManagementModel extends Model
     protected $afterFind      = [];
     protected $beforeDelete   = [];
     protected $afterDelete    = [];
+
+    public function createData($whereData)
+    {
+        return $this->insert($whereData);
+    }
+    public function readData()
+    {
+        return $this->findAll();
+    }
+    public function updateData($whereData, $data)
+    {
+        return $this->where($whereData)->update($data);
+    }
+    public function deleteData($whereData)
+    {
+        return $this->delete($whereData);
+    }
 }
