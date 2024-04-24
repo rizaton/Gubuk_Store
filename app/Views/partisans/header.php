9bf727f46d2069837e59213572bcb1f39d4dc68a
rizaton <123567427+rizaton@users.noreply.github.com>
2024-04-24 17:42:27 +0900

    chore: refactor

--- app/Views/partisans/header.php
+++ app/Views/partisans/header.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="<?= base_url() ?>assets/css/styles.css" rel="stylesheet">
+    <title>Gubuk Store | <?= $title;  ?></title>
 </head>
 
 <body>
--- app/Views/templates/main_member.php
+++ app/Views/templates/main_member.php
@@ -1,5 +1,5 @@
 <?= $this->include('partisans/header');  ?>
-<?= $this->include('partisans/navbar');  ?>
+<?= $this->include('partisans/navbar_member');  ?>
 <div class="content">
     <?= $this->renderSection('content');  ?>
 </div>
