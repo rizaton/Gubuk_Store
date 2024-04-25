196034135f615fc83c00eb70394d8c57c46b423d
rizaton <123567427+rizaton@users.noreply.github.com>
2024-04-26 00:39:51 +0700

    chore: Routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -10,21 +10,26 @@ $routes->get('/', 'Home::index');
 $routes->get('/product', 'Home::products');
 $routes->get('/about', 'Home::about');
 
+// FORGOT
+$routes->get('/forgot', 'Home::forgot_password');
+$routes->post('/f_auth', 'Authentifications::forgot_auth');
+
 // LOGIN
-$routes->get('/login', 'Login::index');
-$routes->post('/l_auth', 'Login::login');
+$routes->get('/login', 'Home::login');
+$routes->post('/l_auth', 'Authentifications::login_auth');
 
 // REGISTER
-$routes->get('/register', 'Login::register');
-$routes->post('/r_auth', 'Login::register');
+$routes->get('/register', 'Home::register');
+$routes->post('/r_auth', 'Authentifications::register_auth');
 
 // MEMBER
 $routes->get('/m/cart', 'Member::cart');
 $routes->get('/m/points', 'Member::points');
 
 // ADMIN
-$routes->get('/a/dashboard', 'Admin::index');
+$routes->get('/a/dashboard', 'Admin::dashboard');
 $routes->get('/a/stocks', 'Admin::stocks');
 $routes->get('/a/members', 'Admin::manage_members');
+$routes->get('/a/promos', 'Admin::promos');
 
 // $routes->setAutoRoute(true);
