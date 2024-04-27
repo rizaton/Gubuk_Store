08f9180950ba67ebc201a64bbdf4dfe01fef1c25
rizaton <123567427+rizaton@users.noreply.github.com>
2024-04-27 16:56:27 +0700

    feat(controller): Home; chore: Helpers(custom)

--- app/Helpers/custom.php
+++ app/Helpers/custom.php
@@ -2,15 +2,39 @@
 
 namespace App\Helpers;
 
-function loggedCheck()
+use CodeIgniter\HTTP\RedirectResponse;
+
+function logged_check(): RedirectResponse
 {
     if (!session()->get('user_data')) {
+        session()->setFlashdata('log_message', 'Harap lakukan login terlebih dahulu');
         return redirect()->to(base_url('l_auth'));
     }
-    return session()->get('access');
+    if (session()->get('user_data')['access'] == 'm') {
+        session()->setFlashdata('log_message', 'Selamat datang kembali');
+        return redirect()->to(base_url(session()->getFlashdata('page')));
+    }
+    if (session()->get('user_data')['access'] == 'a') {
+        session()->setFlashdata('log_message', 'Selamat datang');
+        return redirect()->to(base_url('a/dashboard'));
+    }
+    session()->setFlashdata('log_message', 'Gagal Login');
+    return redirect()->to(base_url(session()->getFlashdata('page')));
 }
-
-function logout()
+function set_login(bool $data = null)
+{
+    session()->setFlashdata('login', $data);
+    return session()->getFlashdata('login');
+}
+function log_status(): bool
+{
+    return session()->get('login');
+}
+function user_access(): string
+{
+    return session()->getFlashdata('user_data')['access'];
+}
+function logout(): RedirectResponse
 {
     session()->remove('login');
     return redirect()->to(base_url(session()->getFlashdata('page')));
