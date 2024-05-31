a38e577254077b11a26028ebce9dc6935306fcc4
FikalAhmad <fikaalm26@gmail.com>
2024-05-31 10:41:17 +0700

    style: add scrollable area in table and add search input

--- app/Views/admin/promos.php
+++ app/Views/admin/promos.php
@@ -1,12 +1,18 @@
 <?= $this->extend('templates/main_admin');  ?>
 
 <?= $this->section('content');  ?>
-<div>
-    <div>
-        <div role="tablist" class="tabs tabs-bordered p-5">
+<div class="bg-base-200 p-5">
+    <div class="p-5 bg-base-100 border-2 border-gray-700 rounded-xl">
+        <div role="tablist" class="tabs tabs-bordered">
             <input type="radio" name="my_tabs_1" role="tab" class="tab" aria-label="Aktif" checked />
-            <div role="tabpanel" class="tab-content p-10">
-                <div class="overflow-x-auto">
+            <div role="tabpanel" class="tab-content">
+                <label class="input input-bordered flex items-center gap-2 mt-10">
+                    <input type="text" class="grow" placeholder="Search" />
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="w-4 h-4 opacity-70">
+                        <path fill-rule="evenodd" d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z" clip-rule="evenodd" />
+                    </svg>
+                </label>
+                <div class="overflow-x-auto mt-5 max-h-[55vh]"> <!-- Table -->
                     <table class="table">
                         <!-- head -->
                         <thead>
@@ -74,8 +80,14 @@
             </div>
 
             <input type="radio" name="my_tabs_1" role="tab" class="tab" aria-label="Tidak Aktif" />
-            <div role="tabpanel" class="tab-content p-10">
-                <div class="overflow-x-auto">
+            <div role="tabpanel" class="tab-content">
+                <label class="input input-bordered flex items-center gap-2 mt-10">
+                    <input type="text" class="grow" placeholder="Search" />
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="w-4 h-4 opacity-70">
+                        <path fill-rule="evenodd" d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z" clip-rule="evenodd" />
+                    </svg>
+                </label>
+                <div class="overflow-x-auto mt-5 max-h-[55vh]"> <!-- Table -->
                     <table class="table">
                         <!-- head -->
                         <thead>
